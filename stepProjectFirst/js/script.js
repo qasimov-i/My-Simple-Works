5f7ad57f6fbe370b27eb7a4ccde3aef74b493d33
Ismayil <isiqasimov93@gmail.com>
2019-11-21 00:51:10 +0400

    step  project

--- stepProjectFirst/js/script.js
+++ stepProjectFirst/js/script.js
@@ -1,9 +1,161 @@
-
-let activeTriangle = document.querySelector(".triangle-active");
-let optionTabsItem = document.getElementsByClassName(".option-tabs-item");
-optionTabsItem.click(function(target){
-    console.log("salam");
+        /****Tabs for information****/
+$('.tab-active').css({
+    backgroundColor:"#18cfab"
 });
+
+let tabBackGraund = function () {
+    $('.option-tabs-item').css({
+        backgroundColor:" #f8fcfe",
+    borderTop:" 1px   solid #ebecec",
+    borderBottom:" 1px   solid #ebecec",
+    borderRight:" 1px solid #ebecec",
+
+    })
+    $('.tab-text').css({
+        color:"#959697"
+    })
+};
+
+let informationShow = function () {
+   $('.options-tabs-information-item').hide();
+};
+let triangleShow = function (){
+    $('.triangle').hide();
+};
+
+$('.tab-first').click(function(){
+    triangleShow();
+    tabBackGraund();
+    informationShow();
+    $('.tab-first').css({
+        borderLeft: "px solid #18cfab",
+    backgroundColor:" #18cfab",
+    borderTop:" 1px   solid #18cfab",
+    borderBottom:" 1px   solid #18cfab",
+    borderRight: "px solid #18cfab"
+    })
+    $('.tab-text-first').css({
+        color:"white"
+    })
+
+    $('.information-first').css({
+        display:"flex"
+    })
+    $('.triangle-first').show();
+});
+
+$('.tab-second').click(function(){
+    triangleShow();
+    tabBackGraund();
+    informationShow();
+    $('.tab-second').css({
+        borderLeft: "px solid #18cfab",
+        backgroundColor:" #18cfab",
+        borderTop:" 1px   solid #18cfab",
+        borderBottom:" 1px   solid #18cfab",
+        borderRight: "px solid #18cfab"
+    })
+
+    $('.information-second').css({
+        display:"flex"
+    })
+    $('.tab-text-second').css({
+        color:"white"
+    })
+    $('.triangle-second').show();
+
+});
+
+
+$('.tab-third').click(function(){
+    triangleShow();
+    tabBackGraund();
+    informationShow();
+    $('.tab-third').css({
+        borderLeft: "px solid #18cfab",
+        backgroundColor:" #18cfab",
+        borderTop:" 1px   solid #18cfab",
+        borderBottom:" 1px   solid #18cfab",
+        borderRight: "px solid #18cfab"
+    })
+    $('.information-third').css({
+        display:"flex"
+    })
+    $('.tab-text-third').css({
+        color:"white"
+    })
+    $('.triangle-third').show();
+
+});
+
+        $('.tab-fourth').click(function(){
+            triangleShow();
+            tabBackGraund();
+            informationShow();
+            $('.tab-fourth').css({
+                borderLeft: "px solid #18cfab",
+                backgroundColor:" #18cfab",
+                borderTop:" 1px   solid #18cfab",
+                borderBottom:" 1px   solid #18cfab",
+                borderRight: "px solid #18cfab"
+            })
+            $('.information-fourth').css({
+                display:"flex"
+            })
+            $('.tab-text-fourth').css({
+                color:"white"
+            })
+            $('.triangle-fourth').show();
+
+        });
+
+ $('.tab-fiveth').click(function(){
+            triangleShow();
+            tabBackGraund();
+            informationShow();
+            $('.tab-fiveth').css({
+                borderLeft: "px solid #18cfab",
+                backgroundColor:" #18cfab",
+                borderTop:" 1px   solid #18cfab",
+                borderBottom:" 1px   solid #18cfab",
+                borderRight: "px solid #18cfab"
+            })
+            $('.information-fiveth').css({
+                display:"flex"
+            })
+            $('.tab-text-fiveth').css({
+                color:"white"
+            })
+            $('.triangle-fiveth').show();
+
+        });
+
+
+        $('.tab-sixth').click(function(){
+            triangleShow();
+            tabBackGraund();
+            informationShow();
+            $('.tab-sixth').css({
+                borderLeft: "px solid #18cfab",
+                backgroundColor:" #18cfab",
+                borderTop:" 1px   solid #18cfab",
+                borderBottom:" 1px   solid #18cfab",
+                borderRight: "px solid #18cfab"
+            })
+            $('.information-sixth').css({
+                display:"flex"
+            })
+            $('.tab-text-sixth').css({
+                color:"white"
+            })
+            $('.triangle-sixth').show();
+ });
+
+
+
+
+
+
 
 
 $('.slick-bigger').slick({
---
+++ stepProjectFirst/js/script2.js
@@ -0,0 +1,124 @@
+
+$('.our-amazing-work-images-item').hide();
+$('.item-all').show();
+
+
+ function $hideTopBorder () {
+    $('.our-amazing-work-tabs-item').css({
+        borderTop: "1px solid #ebebeb"
+    });
+    $('.gallery-tab-text').css({
+        color:"#717171"
+    })
+};
+
+
+
+ $('.tab-all').click(function(){
+     $hideTopBorder();
+     $('.tab-all').css({
+         borderTop:"2px solid  #18cfab",
+     })
+     $('.gallery-tab-text-all').css({
+         color:" #18cfab"
+     })
+     $('.our-amazing-work-images-item').hide();
+     $('.item-all').show();
+
+     $('.button-load-more').show();
+
+ })
+
+
+$('.tab-graphic').click(function (event){
+    $hideTopBorder();
+    $('.tab-graphic').css({
+        borderTop:"2px solid  #18cfab",
+    })
+        $('.gallery-tab-text-graphic').css({
+            color:" #18cfab"
+        })
+    $('.our-amazing-work-images-item').hide();
+    $('.item-graphic').show();
+});
+
+$('.tab-veb').click(function (event){
+    $hideTopBorder();
+    $('.tab-veb').css({
+        borderTop:"2px solid  #18cfab",
+    })
+    $('.gallery-tab-text-veb').css({
+        color:" #18cfab"
+    })
+    $('.our-amazing-work-images-item').hide();
+    $('.item-veb').show();
+});
+
+$('.tab-landing').click(function (event){
+    $hideTopBorder();
+    $('.tab-landing').css({
+        borderTop:"2px solid  #18cfab",
+    })
+    $('.gallery-tab-text-landing').css({
+        color:" #18cfab"
+    })
+    $('.our-amazing-work-images-item').hide();
+    $('.item-landing').show();
+});
+
+$('.tab-word-press').click(function (event){
+    $hideTopBorder();
+    $('.tab-press').css({
+        borderTop:"2px solid  #18cfab",
+    })
+    $('.gallery-tab-text-press').css({
+        color:" #18cfab"
+    })
+    $('.our-amazing-work-images-item').hide();
+    $('.item-press').show();
+});
+
+
+
+/*********load more button***************/
+
+$('.button-load-more').click(function(){
+    let $img1 = $('<img src="img/Layer%207@1X.png" class=our-amazing-work-images-item>');
+    let $img2 = $('<img src="img/Layer%207@1X.png" class=our-amazing-work-images-item>');
+    let $img3 = $('<img src="img/Layer%207@1X.png" class=our-amazing-work-images-item>');
+    let $img4 = $('<img src="img/Layer%207@1X.png" class=our-amazing-work-images-item>');
+    let $img5 = $('<img src="img/Layer%207@1X.png" class=our-amazing-work-images-item>');
+    let $img6 = $('<img src="img/Layer%207@1X.png" class=our-amazing-work-images-item>');
+    let $img7 = $('<img src="img/Layer%207@1X.png" class=our-amazing-work-images-item>');
+    let $img8 = $('<img src="img/Layer%207@1X.png" class=our-amazing-work-images-item>');
+    let $img9 = $('<img src="img/Layer%207@1X.png" class=our-amazing-work-images-item>');
+    let $img10 = $('<img src="img/Layer%207@1X.png" class=our-amazing-work-images-item>');
+    let $img11 = $('<img src="img/Layer%207@1X.png" class=our-amazing-work-images-item>');
+    let $img12 = $('<img src="img/Layer%207@1X.png" class=our-amazing-work-images-item>');
+    $('.column-first').append($img1,$img2,$img3);
+    $('.column-second').append($img4,$img5,$img6);
+    $('.column-third').append($img7,$img8,$img9);
+    $('.column-fourth').append($img10,$img11,$img12);
+    $('.button-load-more').hide();
+});
+
+
+
+
+
+
+
+/***** our amazzing work : picture hover *****/
+
+$('.our-amazing-work-images-item').mouseover(function (){
+
+    let $moreInfo = $("<div class ='more-info-img'></div>");
+    $(this).before($moreInfo);
+    $(this).hide();
+
+    $moreInfo.css({
+        backgroundColor:"red",
+        width: "290px",
+        height: "200px",
+    })
+});
