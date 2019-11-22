d4489b575f49e1f3686df5db3e10edbb7d798ac2
Ismayil <isiqasimov93@gmail.com>
2019-11-22 13:28:27 +0400

    ham

--- stepProjectFirst/js/script.js
+++ stepProjectFirst/js/script.js
@@ -3,11 +3,6 @@ $('.tab-active').css({
     backgroundColor:"#18cfab"
 });
 
-
-
-
-
-
 let tabBackGraund = function () {
     $('.option-tabs-item').css({
         backgroundColor:" #f8fcfe",
@@ -187,3 +182,45 @@ slickSmallerItems.forEach(element=>{
 
 
 /***** picture date******/
+let date = new Date();
+
+        $('.calendar-day').text(date.getDate());
+        let month = date.getMonth();
+        switch(month){
+            case 0:
+                $('.calendar-month').text("Jan");
+                break;
+            case 1:
+                $('.calendar-month').text("Feb");
+                break;
+            case 2:
+                $('.calendar-month').text("Mar");
+                break;
+            case 3:
+                $('.calendar-month').text("Apr");
+                break;
+            case 4:
+                $('.calendar-month').text("May");
+                break;
+            case 5:
+                $('.calendar-month').text("Jun");
+                break;
+            case 6:
+                $('.calendar-month').text("Jul");
+                break;
+            case 7:
+                $('.calendar-month').text("Aug");
+                break;
+            case 8:
+                $('.calendar-month').text("Sem");
+                break;
+            case 9:
+                $('.calendar-month').text("Oct");
+                break;
+            case 10:
+                $('.calendar-month').text("Now");
+                break;
+            case 11:
+                $('.calendar-month').text("Dec");
+                break;
+        }
